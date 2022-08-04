0a7452d6b907bc164e4d9f07aa52b0aa1b58123c
ElJoamy <joamysalguero1@gmail.com>
2022-08-03 20:02:28 -0400

    chattbot con python

---
+++ chatapp.py
@@ -0,0 +1,63 @@
+import nltk, json, random, pickle
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+from tensorflow.keras.models import load_model
+
+model = load_model('chatbot_model.h5')
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+
+# preprocesamiento de la entrada del usuario
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# creacion bag of words
+def bow(sentence, words, show_details=True):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def calcula_prediccion(sentence, model):
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # ordenar por la fuerza de probabilidad
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getRespuesta(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def start(msg):
+    ints = calcula_prediccion(msg, model)
+    res = getRespuesta(ints, intents)
+    return res
+
+usuario = ''
+print('Bienvenido. Para salir, escriba "Salir"')
+
+while usuario != 'Salir':
+    usuario = str(input(""))
+    res = start(usuario)
+    print('AI:' + res)
